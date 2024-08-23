95bc044f1c7b0b364eae52438101ed7c3eb5dca8
Ezgi Kececi Atalay <ezgikececii@gmail.com>
2024-08-23 09:47:49 +0300

    1.Odev.sql

--- 1.Odev.sql
+++ 1.Odev.sql
@@ -1,21 +1,20 @@
 --1. Uygulama
---SELECT title, description FROM film;
+SELECT title, description FROM film;
 
 --2.Uygulama
---SELECT * 
---FROM film 
---WHERE length > 60 AND length <75 ;
+SELECT * 
+FROM film 
+WHERE length > 60 AND length <75 ;
 
 --3.Uygulama
-----SELECT * 
---FROM film
---WHERE rental_rate =0.99 AND (replacement_cost= 12.99 OR replacement_cost =28.99);
+SELECT * 
+FROM film
+WHERE rental_rate =0.99 AND (replacement_cost= 12.99 OR replacement_cost =28.99);
 
 --4.Uygulama
---SELECT last_name 
---FROM customer
--- first_name = 'Mary';
-
+SELECT last_name 
+FROM customer first_name = 'Mary';
+ 
 --5.Uygulama
 SELECT * 
 FROM film 
